8e9f4284a10e5e46bc33705143825200f2038757
Virginia Dooley <vcdooley@gmail.com>
2018-09-11 13:39:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(name, days) {
-  return ("It's ${days} days until ${holiday}!");
+  return ("It\'s ${days} days until ${holiday}!");
 }
