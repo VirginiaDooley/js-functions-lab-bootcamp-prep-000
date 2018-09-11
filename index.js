0ade38563093add772333be85a38e61bb77b575f
Virginia Dooley <vcdooley@gmail.com>
2018-09-11 13:25:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function happyHolidays() {
 /*This function should not accept any parameters and should return the string "Happy holidays!"*/
 }
 
+function happyHolidayTo() {
+}
+
 function happyHolidayTo(name) {
   return ('Happy holidays, ${name}!');
 }
