30ddd1e93b52af9d6aee10f73c2730fb7b525339
Virginia Dooley <vcdooley@gmail.com>
2018-09-11 13:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
   return ('Happy holidays, ${name}!');
 }
 
-function happyHolidaysTo(name, holiday) {
+function happyHolidayTo(name, holiday) {
   return ('Happy ${holiday}, ${name}');
 }
