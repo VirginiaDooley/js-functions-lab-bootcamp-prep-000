68c9e4b8904a31a58fb94f9b51befdf57e4fdf2b
Virginia Dooley <vcdooley@gmail.com>
2018-09-11 13:33:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // write your code below!
-function 
-
 function happyHolidays() {
   return ('Happy holidays!')
 /*This function should not accept any parameters and should return the string "Happy holidays!"*/
